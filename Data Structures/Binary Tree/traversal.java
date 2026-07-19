b127afe0e25d19b442ae5e4497e52f7a3092d818
adderlyjc <ajacs1104@gmail.com>
2026-07-19 08:30:32 -0600

    Initial commit

---
+++ Data Structures/Binary Tree/traversal.java
@@ -0,0 +1,11 @@
+class TreeNode<T> {
+    T data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
